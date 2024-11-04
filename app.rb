f0ea3f1226180b34825c29df15fbd8b065d2258a
Martin Terner <martin.terner@elev.ga.ntig.se>
2024-11-04 13:00:55 +0100

    login page added

--- app.rb
+++ app.rb
@@ -1,7 +1,72 @@
+require 'bcrypt'
+
 class App < Sinatra::Base
 
-    get '/' do
-        erb(:"index")
+    def logged_in?
+        !session[:user_id].nil?
     end
 
+    get '/' do
+        redirect '/home'
+    end
+
+    get '/home' do 
+        erb(:"home")
+    end
+
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/fruits.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
+    before do
+        @user = db.execute("SELECT * FROM users WHERE username = ?", [session['user_id']]).first
+
+        unless @user.nil? || request.path_info == '/login' || request.path_info == '/register'
+            redirect '/login'
+        end
+    end
+
+    get '/login' do
+        erb :"login", :layout => false
+    end
+
+    get '/register' do
+        erb :"register", :layout => false
+    end
+
+    post '/login' do
+        content_type :json
+
+        request_payload = JSON.parse request.body.read
+
+        username = request_payload["usernameInput"]
+        password = request_payload["passwordInput"]
+
+        
+        user = db.execute("SELECT * FROM users WHERE username = ?", [username]).first
+        
+        p user
+
+        return { status: 'error', message: 'Incorrect password or username' }.to_json unless user
+
+        if user['verified'] == 0 
+            { status: 'error', message: 'Account is not verified, please check your mail' }.to_json
+        end
+
+        stored_password_hash = user['password_hash']
+
+        if BCrypt::Password.new(stored_password_hash) == password
+            session[:user_id] = username
+            p "redirect"
+            return { status: 'success', redirect_url: '/home', message: 'Redirecting to /home' }.to_json
+            # redirect '/home'
+        end
+
+        { status: 'error', message: 'Incorrect password or username' }.to_json
+    end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,11 +1,42 @@
 require 'sqlite3'
+require 'bcrypt'
 
 class Seeder
-
+  
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE users (
+                id INTEGER PRIMARY KEY,
+                username TEXT UNIQUE NOT NULL,
+                password_hash TEXT NOT NULL,
+                email TEXT UNIQUE NOT NULL,
+                verification_token TEXT,
+                verified INTEGER DEFAULT 0 NOT NULL,
+                admin INTEGER DEFAULT 0 NOT NULL)')
+  end
+
+  def self.populate_tables
+    password_hash = BCrypt::Password.create('SecretPassword')
+    db.execute("INSERT INTO users (username, password_hash, email, verified, admin) VALUES (?, ?, ?, ?, ?)", ['oudend', password_hash, "oudend@gmail.com", 1, 1])
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/fruits.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
 
 Seeder.seed!
